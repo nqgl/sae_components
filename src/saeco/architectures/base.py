c6c5b345d63c3cc684775affb2187278cd59710c
Glen Taggart <glenmtaggart@gmail.com>
2024-06-11 18:36:20 -0700

    refactor: Add SAE base architecture and related components

---
+++ src/saeco/architectures/base.py
@@ -0,0 +1,109 @@
+import torch
+import torch.nn as nn
+from abc import ABC, abstractmethod
+from torch import Tensor
+from jaxtyping import Float
+
+import saeco.core as cl
+import saeco.components as cc
+
+from saeco.components import (
+    Penalty,
+    L1Penalty,
+    FreqTracked,
+    EMAFreqTracker,
+    FreqTracker,
+    ResampledLayer,
+    Loss,
+    L2Loss,
+    SparsityPenaltyLoss,
+    SAECache,
+)
+import saeco.core.module
+from saeco.core.linear import Bias, NegBias, Affine, MatMul
+from typing import Optional, Protocol, runtime_checkable
+import saeco.components as co
+
+
+class LinearLike(Protocol):
+    weight: Tensor
+    bias: Tensor
+
+
+class SAE(cl.Seq):
+    encoder: cl.Module
+    penalty: cl.Module
+    decoder: cl.Module
+    metrics: co.metrics.ActMetrics
+    freq_tracker: co.FreqTracker
+
+    def __init__(
+        self,
+        pre_bias,
+        encoder,
+        decoder,
+        b_dec=None,
+        penalty=True,
+        metrics=None,
+        freq_tracker=None,
+    ):
+        self._b_dec = b_dec
+        super().__init__(
+            **(
+                {}
+                if pre_bias is False
+                else {"pre_bias": pre_bias or cl.ops.Sub(self.b_dec)}
+            ),
+            encoder=encoder,
+            **(
+                {}
+                if penalty is None
+                else dict(penalty=(L1Penalty() if penalty is True else penalty))
+            ),
+            freq_tracker=freq_tracker or EMAFreqTracker(),
+            metrics=metrics or co.metrics.ActMetrics(),
+            decoder=decoder,
+        )
+
+    @property
+    def b_dec(self):
+        if self._b_dec is None:
+            return self.decoder.bias
+        return self._b_dec
+
+    @property
+    def freqs(self): ...
+
+    def encode(self, x):
+        if hasattr(self, "pre_bias"):
+            x = self.pre_bias(x)
+        return self.encoder(x)
+
+    def decode(self, x):
+        if self._b_dec is None:
+            return self.decoder(x)
+        else:
+            return self.decoder(x) + self.b_dec
+
+    def cent(self, x):
+        if hasattr(self, "pre_bias"):
+            return self.pre_bias(x)
+        raise NotImplementedError
+
+    def uncent(self, x):
+        return x + self.b_dec
+
+    def enc(self, x, *, cache: SAECache = None):
+        if cache is None:
+            cache = SAECache()
+        return cache(self).encoder(x)
+        # return self.encoder(x, cache=cache)
+
+    def dec(self, x, *, cache: SAECache = None):
+        if cache is None:
+            cache = SAECache()
+        if self._b_dec is None:
+            W = self.decoder.weight
+            return torch.nn.functional.linear(x, W, None)
+        return cache(self).decoder(x)
+        # return self.decoder(x, cache=cache)
--- src/saeco/architectures/vanilla_tests.py
+++ src/saeco/architectures/vanilla_tests.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 from torch import Tensor
 from jaxtyping import Float
 
+from saeco.architectures.initialization.initializer import Initializer
 from saeco.components.ops.detach import Thresh
 import saeco.core as cl
 import saeco.core.module
@@ -30,7 +31,7 @@ from saeco.core import Seq
 import saeco.components.features.features as ft
 import saeco.components as co
 from saeco.trainer.trainable import Trainable
-from saeco.architectures.tools import bias, weight, mlp_layer, Initializer
+from saeco.architectures.initialization.tools import bias, weight, mlp_layer
 
 
 # from torch.utils.viz._cycles import
@@ -125,6 +126,32 @@ def basic_vanilla_sae_lin(init: Initializer):
         freqs=EMAFreqTracker(),
         L1=L1Penalty(),
         metrics=co.metrics.ActMetrics(),
+        decoder=ft.OrthogonalizeFeatureGrads(
+            ft.NormFeatures(
+                init.decoder,
+            ),
+        ),
+    )
+
+    models = [model]
+    losses = dict(
+        L2_loss=L2Loss(model),
+        sparsity_loss=SparsityPenaltyLoss(model),
+    )
+    return models, losses
+
+
+def classed_basic_vanilla_sae_lin(init: Initializer):
+    # model
+    from saeco.architectures.base import SAE
+
+    model = SAE(
+        pre_bias=Sub(init.decoder.bias),
+        encoder=Seq(
+            lin=init.encoder,
+            nonlinearity=nn.ReLU(),
+        ),
+        penalty=L1Penalty(),
         decoder=ft.OrthogonalizeFeatureGrads(
             ft.NormFeatures(
                 init.decoder,
