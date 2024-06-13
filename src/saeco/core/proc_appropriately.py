4c3a152d7c12b5531d0e046b53fa8ee627d739da
Glen Taggart <glenmtaggart@gmail.com>
2024-06-13 15:46:55 -0700

    added debugging feature (WIP) that locates the exception in the cache tree

--- src/saeco/core/proc_appropriately.py
+++ src/saeco/core/proc_appropriately.py
@@ -5,12 +5,21 @@ import torch
 import torch.nn as nn
 
 
+CE_STR = "CacheExceptionLocationInfo: Except inside of subcache path: "
+
+
 def proc_appropriately(module, name, x, cache: Cache, **kwargs):
-    if isinstance(module, Module):
-        val = module(x, cache=cache[name], **kwargs)
-    elif isinstance(module, nn.Parameter):  # | torch.Tensor?
-        return module
-    else:
-        val = module(x)
-    cache._write(name, val)
+    try:
+        if isinstance(module, Module):
+            val = module(x, cache=cache[name], **kwargs)
+        elif isinstance(module, nn.Parameter):  # | torch.Tensor?
+            return module
+        else:
+            val = module(x)
+        cache._write(name, val)
+    except Exception as e:
+        if not any([CE_STR in a for a in e.args]):
+            e.args = e.args + (f"{CE_STR}{cache._name}.{name}",)
+
+        raise e
     return val
