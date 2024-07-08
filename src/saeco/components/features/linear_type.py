c3017c7c48ba73a6442ba01a0244eac3c6d19c14
Glen Taggart <glenmtaggart@gmail.com>
2024-07-07 17:14:14 -0700

    make features for linear resampling compatible + beginning to break features into files

---
+++ src/saeco/components/features/linear_type.py
@@ -0,0 +1,73 @@
+from saeco.components.features.features import HasFeatures, ResampledWeight, Resamplable
+from saeco.components.wrap import WrapsModule
+from typing import Any, Optional
+
+import torch.nn as nn
+from torch import Tensor
+
+
+from abc import abstractmethod
+
+
+class LinWeights(WrapsModule):
+    wrapped: nn.Linear
+
+    def __init__(self, wrapped: nn.Linear):
+        assert isinstance(wrapped, nn.Linear)
+        super().__init__(wrapped)
+
+    @abstractmethod
+    def features_transform(self, tensor: Tensor) -> Tensor: ...
+
+    @property
+    def features(self) -> Tensor:
+        f = self.features_transform(self.wrapped.weight.data)
+        assert f.shape[0] != 768
+        return f
+
+    @property
+    def features_grad(self) -> Optional[Tensor]:
+        grad = self.wrapped.weight.grad
+        if grad is None:
+            return None
+        f = self.features_transform(grad)
+        assert f.shape[0] != 768
+        return f
+
+    def resampled(self):
+        return ResampledWeight(self)  # TODO bias res too
+
+
+class LinDecoder(LinWeights):
+    def features_transform(self, tensor: Tensor) -> Tensor:
+        return tensor.transpose(-2, -1)
+
+
+class LinEncoder(LinWeights):
+    def features_transform(self, tensor: Tensor) -> Tensor:
+        return tensor
+
+
+class ResampledLinEnc(ResampledWeight):
+    wrapped: LinEncoder
+
+    def __init__(self, wrapped: LinEncoder):
+        assert isinstance(wrapped, LinEncoder)
+        super().__init__(wrapped)
+
+    def resample(self, *, indices, new_directions, bias_reset_value):
+        super().resample(  # do weights
+            indices=indices,
+            new_directions=new_directions,
+            bias_reset_value=bias_reset_value,
+        )
+        if not self.bias is None:
+            self.bias.data[indices] = bias_reset_value
+
+
+class LinEncoder(LinWeights):
+    def features_transform(self, tensor: Tensor) -> Tensor:
+        return tensor
+
+    def resampled(self):
+        return ResampledLinEnc(self)
---
+++ src/saeco/components/features/matmul_type.py
@@ -0,0 +1,49 @@
+import saeco.core as cl
+from saeco.components.features.features import ResampledWeight
+from saeco.components.wrap import WrapsModule
+from typing import Optional
+
+import torch.nn as nn
+from torch import Tensor
+
+
+from abc import abstractmethod
+
+
+class MatMulWeights(WrapsModule):
+    wrapped: cl.ops.MatMul
+
+    def __init__(self, wrapped: cl.ops.MatMul):
+        if isinstance(wrapped, nn.Parameter):
+            wrapped = cl.ops.MatMul(wrapped)
+        assert isinstance(wrapped, cl.ops.MatMul)
+        super().__init__(wrapped)
+
+    @abstractmethod
+    def features_transform(self, tensor: Tensor) -> Tensor: ...
+
+    @property
+    def features(self) -> Tensor:
+        return self.features_transform(self.wrapped.right.data)
+
+    @property
+    def features_grad(self) -> Optional[Tensor]:
+        grad = self.wrapped.right.grad
+        if grad is None:
+            return None
+        return self.features_transform(grad)
+
+    def resampled(self):
+        return ResampledWeight(self)
+
+
+class DecoderWeights(MatMulWeights):
+    def features_transform(self, tensor: Tensor) -> Tensor:
+        return tensor
+
+
+class EncoderWeights(MatMulWeights):
+    wrapped: cl.ops.MatMul
+
+    def features_transform(self, tensor: Tensor) -> Tensor:
+        return tensor.transpose(-2, -1)
