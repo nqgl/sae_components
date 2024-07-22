573109887b3522b175ee3c8458668c29386a3dda
Glen Taggart <glenmtaggart@gmail.com>
2024-07-21 23:22:31 -0700

    anth april

--- src/saeco/components/penalties/l1_penalizer.py
+++ src/saeco/components/penalties/l1_penalizer.py
@@ -11,6 +11,25 @@ class L1Penalty(Penalty):
         return x.relu().mean(dim=0).sum() * self.scale
 
 
+class L1PenaltyScaledByDecoderNorm(Penalty):
+    def __init__(self, scale=1.0, decoder=None):
+        super().__init__()
+        self.scale = scale
+        self.decoder = None
+        if decoder is not None:
+            self.set_decoder(decoder)
+
+    def penalty(self, x: Tensor):
+        mean_act = x.mean(dim=0)
+        scaled_acts = self.decoder.features[:].norm(dim=1) * mean_act
+        return scaled_acts.sum() * self.scale
+
+    def set_decoder(self, decoder):
+        assert self.decoder is None
+        self.decoder = decoder.features["weight"]
+        return decoder
+
+
 class L0TargetingL1Penalty(Penalty):
     def __init__(self, target, scale=1.0):
         super().__init__()
