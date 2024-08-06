2ab7b5e3a7b296328052546a187c2fc444b9b0cf
Glen Taggart <glenmtaggart@gmail.com>
2024-08-05 19:51:29 -0700

    updated gated

---
+++ src/saeco/architectures/gated/__init__.py
@@ -0,0 +1,2 @@
+from .gated_model import gated_sae as model_fn
+from .gated_config import cfg, PROJECT
---
+++ src/saeco/architectures/gated/gated_config.py
@@ -0,0 +1,47 @@
+from .gated_model import Config
+from saeco.components.resampling.anthropic_resampling import (
+    AnthResamplerConfig,
+    OptimResetValuesConfig,
+)
+from saeco.data import ActsDataConfig, DataConfig, ModelConfig
+from saeco.sweeps import SweepableConfig, Swept
+from saeco.trainer import RunSchedulingConfig
+from saeco.trainer.runner import RunConfig, TrainConfig
+
+PROJECT = "sae sweeps"
+
+cfg = RunConfig[Config](
+    train_cfg=TrainConfig(
+        data_cfg=DataConfig(
+            model_cfg=ModelConfig(acts_cfg=ActsDataConfig(excl_first=True))
+        ),
+        raw_schedule_cfg=RunSchedulingConfig(
+            run_length=50_000,
+            resample_period=9_000,
+        ),
+        #
+        batch_size=4096,
+        optim=Swept("Adam", "ScheduleFree"),
+        lr=Swept(1e-3, 3e-4, 1e-4, 5e-5),
+        betas=(0.9, 0.999),
+        #
+        use_autocast=True,
+        use_lars=Swept(True, False),
+        #
+        l0_target=25,
+        l0_target_adjustment_size=0.0002,
+        coeffs={
+            "sparsity_loss": 3e-3,
+            "L2_loss": 1,
+        },
+        #
+        wandb_cfg=dict(project=PROJECT),
+    ),
+    resampler_config=AnthResamplerConfig(
+        optim_reset_cfg=OptimResetValuesConfig(),
+        expected_biases=2,
+        # expected_encs=1,
+    ),
+    #
+    arch_cfg=Config(),
+)
