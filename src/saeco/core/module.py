3b78338d158ac3ed926a3dae7fbdf850b3c992d7
Glen Taggart <glenmtaggart@gmail.com>
2025-02-08 13:15:29 -0800

    context managers for eval/train on cl.Module

--- src/saeco/core/module.py
+++ src/saeco/core/module.py
@@ -8,6 +8,7 @@ from abc import ABC, abstractmethod, ABCMeta
 
 
 from typing import Callable, List, Any
+from contextlib import contextmanager
 
 c2t = Callable[..., torch.Tensor | List[torch.Tensor]]
 
@@ -26,3 +27,21 @@ class Module(torch.nn.Module, ABC):
         return super().__instancecheck__(instance) or (
             isinstance(instance, WrapsModule) and isinstance(instance.wrapped, cls)
         )
+
+    @contextmanager
+    def train_ctx(self):
+        prev_state = self.training
+        self.train()
+        try:
+            yield
+        finally:
+            self.train(prev_state)
+
+    @contextmanager
+    def eval_ctx(self):
+        prev_state = self.training
+        self.eval()
+        try:
+            yield
+        finally:
+            self.train(prev_state)
