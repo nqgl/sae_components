a71a5816b23afd36c1b757af5e1e2d3ef34ec353
Glen Taggart <glenmtaggart@gmail.com>
2025-11-15 09:53:00 -0800

    desktop pyproject.toml

--- setup.py
+++ setup.py
@@ -17,10 +17,6 @@ setup(
     install_requires=requirements,
     extras_require={},
     classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
