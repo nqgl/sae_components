01a7ba2cefaba6fb46308ffc75e2a5d088edfdd3
Glen Taggart <glemtaggart@gmail.com>
2025-11-14 19:52:48 -0800

    wip comlm model cfg

--- src/saeco/data/config/model_config/comlm_model_cfg.py
+++ src/saeco/data/config/model_config/comlm_model_cfg.py
@@ -11,16 +11,20 @@ from transformers import (
 )
 
 from saeco.data.config.locations import DATA_DIRS
-
+from typing import Any
 from saeco.data.config.model_config.model_type_cfg_base import ModelLoadingConfigBase
 from comlm.utils import ModelCheckpointIdentifier
 from comlm.architecture import TransformerArchitecture
+from comlm.exprank import XRArch
+from comlm.exprank.xr_transformer import XRTransformer
 from paramsight import takes_alias, get_resolved_typevars_for_base
 from nnsight import NNsight
 
+from saeco.data.dict_batch.dict_batch import DictBatch
 
-class ComlmModelConfig[ArchT: TransformerArchitecture](
-    ModelLoadingConfigBase[PreTrainedModel]
+
+class ComlmModelConfig[ArchT: TransformerArchitecture[Any, Any, Any] = XRArch](
+    ModelLoadingConfigBase[XRTransformer]
 ):
     model_ident: ModelCheckpointIdentifier
 
@@ -35,7 +39,7 @@ class ComlmModelConfig[ArchT: TransformerArchitecture](
     def pretrained_arch(self) -> ArchT:
         arch_cls = self.get_arch_cls()
         print(f"loading {arch_cls} from {self.model_ident.path}")
-        arch_cls.load_from_checkpoint_identifier(
+        return arch_cls.load_from_checkpoint_identifier(
             self.model_ident,
             load_weights_only=True,
             update_arch_before_load=self.update_arch_before_load,
@@ -47,18 +51,22 @@ class ComlmModelConfig[ArchT: TransformerArchitecture](
         )
 
     @property
-    def model_name(self) -> str: ...
+    def model_name(self) -> str:  # type: ignore
+        return self.model_ident.simple_name
 
     @cached_property
-    def tokenizer(self): ...
+    def tokenizer(self):
+        return self.pretrained_arch.tokenizer
 
     def _make_raw_model(
         self,
         load_as_dtype: torch.dtype | None = None,
         device: str | torch.device = "cuda",
-    ): ...
+    ) -> XRTransformer:
+        return self.pretrained_arch.model
 
-    def nnsight_wrap(self, model: PreTrainedModel) -> NNsight: ...
+    def nnsight_wrap(self, model: XRTransformer) -> NNsight:
+        return NNsight(model)
 
     def update_arch_before_load(self, arch: TransformerArchitecture):
         pass
@@ -74,3 +82,25 @@ class ComlmModelConfig[ArchT: TransformerArchitecture](
         #         if rmname in key:
         #             del model_dict[key]
         return state_dict
+
+
+    def input_data_transform(self, input_data: DictBatch) -> DictBatch:
+        
+# train_dataloader_filtered
+# [
+#     train_datagen_unfiltered[
+#         self.run_cfg.train_cfg.data_cfg.get_dataset(
+#                 batch_size=self.device_batch_size,
+#                 num_workers=num_workers,
+#             )
+#         ->
+#         self._add_metadata_processing_to_dataset(
+#             cast(Iterable[dict[str, torch.Tensor]], dataset)
+#         )
+#     ]
+#      ->
+#      _convert_to_filtered_dataloader
+
+# ]
+# ->
+# self.training_microbatch_transform
